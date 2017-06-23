bb2c1eebc2d907a3babe36ff7bd82ad0a42445e4
chris dostert <chris.dostert@nintex.com>
2017-06-22 22:23:04 -0700

    chore) initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# problem statement
+asserts a golang coverprofile meets min coverage
+
+# example usage
+
+> note: in examples, VERSION represents a version of the golang.coverprofile.assert pkg
+
+## install
+
+```shell
+opctl pkg install github.com/opspec-pkgs/golang.coverprofile.assert#VERSION
+```
+
+## run
+
+```
+opctl run github.com/opspec-pkgs/golang.coverprofile.assert#VERSION
+```
+
+## compose
+
+```yaml
+op:
+  pkg: { ref: github.com/opspec-pkgs/golang.coverprofile.assert#VERSION }
+  inputs: 
+    profile:
+    min:
+  outputs: { bin }
+```
---
+++ cmd.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -e
+
+# calculate coverage
+declare -i total_statements total_hits line_statements line_hits
+# lines in form: "location statement_count hit_count"
+while read -r line; do
+  # ignore mode lines
+  if [[ $line != mode* ]] ;
+  then
+    IFS=' ' read -ra line_parts <<< "$line"
+    line_statements=${line_parts[1]}
+    line_hits=${line_parts[2]}
+
+    # increment statements
+    total_statements=$total_statements+$line_statements
+
+    if [[ $line_statements -gt $line_hits ]] ;
+    then
+      total_hits=$total_hits+$line_hits
+    else
+      total_hits=$total_hits+$line_statements
+    fi
+  fi
+done < /profile
+
+coverage=$(($total_hits*100 / $total_statements))
+echo "Coverage info => Actual: $coverage%, Expected: >= $min%"
+
+# enforce minCoverage
+if [[ $coverage -lt $min ]] ;
+then
+  echo "Insufficient test coverage!" 1>&2
+  exit 1
+else
+  echo "Coverage ok"
+fi
